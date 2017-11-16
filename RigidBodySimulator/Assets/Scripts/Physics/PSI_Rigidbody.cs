c2fea27e9968c64bf7d59e159e0a01705a728ec9
Unknown <henrybell96@hotmail.com>
2017-11-16 11:53:20 +0000

    Basic Rigidbody Class

---
+++ RigidBodySimulator/Assets/Scripts/Physics/PSI_Rigidbody.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSI_Rigidbody : MonoBehaviour {
+
+    public float pMass { get { return Mass; } }
+    public float pCoeffOfRest { get { return CoeffOfRest; } }
+
+    public Vector3 Velocity = Vector3.zero;
+    public Vector3 AngularVelocity = Vector3.zero;
+
+    [SerializeField]
+    [Range(1.0f, 20.0f)]
+    private float Mass = 1.0f;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    [Tooltip("The coefficient of restitution for the body")]
+    private float CoeffOfRest = 0.5f;
+
+    [SerializeField]
+    private bool UseGravity = true;
+
+    private Vector3 mSumForce = Vector3.zero;
+
+
+    //--------------------------------------Unity Functions--------------------------------------
+
+    private void Update()
+    {
+        // Applying angular movement.
+        this.transform.Rotate(this.AngularVelocity * Time.deltaTime);
+
+        // Applying linear movement.
+        var acceleration = this.mSumForce / this.Mass;
+        this.Velocity += acceleration * Time.deltaTime;
+        if (this.UseGravity) this.Velocity.y += Physics.gravity.y * Time.deltaTime;
+        this.transform.Translate(this.Velocity * Time.deltaTime, Space.World);
+
+        // Reseting variables.
+        this.mSumForce = Vector3.zero;
+    }
+
+
+    //-------------------------------------Public Functions-------------------------------------
+
+    public void AddForce(Vector3 force)
+    {
+        this.mSumForce += force;
+    }
+}
